5fbc376199475a0ec5c69a5446f298daf8aca07e
ivanpaulovich <ivan@100loop.com>
2018-02-14 12:09:59 -0200

    Register response improved

--- source/WebAPI/Acerola.UI/Controllers/CustomersController.cs
+++ source/WebAPI/Acerola.UI/Controllers/CustomersController.cs
@@ -8,6 +8,7 @@
     using Acerola.UI.Requests;
     using Acerola.Application.Commands.Register;
     using Acerola.UI.Model;
+    using System.Collections.Generic;
 
     [Route("api/[controller]")]
     public class CustomersController : Controller
@@ -29,12 +30,33 @@
         public async Task<IActionResult> Post([FromBody]RegisterRequest request)
         {
             var command = new RegisterCommand(request.PIN, request.Name, request.InitialAmount);
-            RegisterResult registerResult = await registerService.Handle(command);
+            RegisterResult result = await registerService.Handle(command);
+
+            List<TransactionModel> transactions = new List<TransactionModel>();
+
+            foreach (var item in result.Account.Transactions)
+            {
+                var transaction = new TransactionModel(
+                    item.Amount,
+                    item.Description,
+                    item.TransactionDate);
+
+                transactions.Add(transaction);
+            }
+
+            AccountDetailsModel account = new AccountDetailsModel(
+                result.Account.AccountId,
+                result.Account.CurrentBalance,
+                transactions);
+
+            List<AccountDetailsModel> accounts = new List<AccountDetailsModel>();
+            accounts.Add(account);
 
             RegisterModel model = new RegisterModel(
-                registerResult.Customer.CustomerId,
-                registerResult.Customer.Personnummer,
-                registerResult.Customer.Name
+                result.Customer.CustomerId,
+                result.Customer.Personnummer,
+                result.Customer.Name,
+                accounts
             );
 
             return CreatedAtRoute("GetCustomer", new { id = model.CustomerId }, model);
